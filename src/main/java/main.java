69137c7637175b87da9c91231136699d41d86030
Maciek Podgórzak <impimp94@gmail.com>
2016-11-12 22:25:39 +0100

    Poprawka FX

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,7 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import pl.ether.Hello.Hello;
-
+import pl.ether.forms.MainForm;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,31 +16,21 @@ import javafx.stage.Stage;
 /**
  * Created by nikk on 09.11.16.
  */
-public class main extends Application {
+public class main{
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+   
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-//    	ClassLoader cl = ClassLoader.getSystemClassLoader();
-//    	URL[] url = ((URLClassLoader)cl).getURLs();
-//    	for (URL u : url) {
-//    		System.out.println(u);
-//    	}
-//        ApplicationContext ctx = new ClassPathXmlApplicationContext("SpringBeans.xml");
 
-//        Hello bean = (Hello) ctx.getBean("HelloBean2");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("SpringBeans.xml");
+        
+        MainForm mf = (MainForm) ctx.getBean("MainWindow");
+        mf.run(args);
 
-//        bean.sayHello();
 
-        launch(args);
+        
     }
 
 }
