d2401076c74a01a7baddbee730e0acd0f512a63b
Chorss <kacper.czarczynski@gmail.com>
2016-11-12 21:21:45 +0100

    add JavaFX

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,11 +1,36 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by nikk on 09.11.16.
  */
-public class main {
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Hello2");
+//    	ClassLoader cl = ClassLoader.getSystemClassLoader();
+//    	URL[] url = ((URLClassLoader)cl).getURLs();
+//    	for (URL u : url) {
+//    		System.out.println(u);
+//    	}
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("SpringBeans.xml");
+
+//        Hello bean = (Hello) ctx.getBean("HelloBean2");
+
+//        bean.sayHello();
+
+        launch(args);
     }
 
 }
